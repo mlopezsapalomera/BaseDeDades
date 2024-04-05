ed368d99c393f010f1895dd6911630463d093ee7
mlopezsapalomera <164485513+mlopezsapalomera@users.noreply.github.com>
2024-04-05 13:32:45 +0200

    Create Exercicis RRHH.sql

---
+++ Exercicis /Exercicis RRHH.sql
@@ -0,0 +1,46 @@
+/*ENUNCIATS DE FUNCIONS*/
+
+/*
+Exercici 1 - Fes una funció anomenada spData, tal que donada una data en format
+MySQL ( AAAA-MM-DD ) ens retorni una cadena de caràcters en format DD-MM-AAAA
+Exemple : SELECT spData('1988-12-01') => 01-12-1988
+
+Exercici 2 - Fes una funció anomenada spPotencia, tal que donada una base i un
+exponent, ens calculi la seva potència. Intenta no utilitzar la funció POW.
+
+Exemple : SELECT spPotencia(2,3) => 8
+
+
+Exercici 3 - Fes una funció anomenada spIncrement que donat un codi d’empleat i un
+% de increment, ens calculi el salari sumant aquest percentatge.
+Per exemple, suposem que l’ empleat amb id_empleat = 124 té un salari de 1000
+
+Exemple: SELECT spIncrement(124,10) obtindriem -> 1100
+
+
+Exercici 4 - Fes una funció anomenada spPringat, tal que li passem un codi de
+departament, i ens torni el codi d’empleat que guanya menys d’aquell departament.
+
+
+Exercici 5 - Utilitzant la funció spPringat fes una consulta per obtenir de cada
+departament, l’empleat pringat. Mostra el codi i nom del departament, i el codi d’empleat.
+
+
+Exercici 6 - Fes una funció anomenada spCategoria, tal que donat un codi d’empleat,
+ens digui en quina categoria professional està. El criteri que volem seguir per determinar
+la categoria professional és en funció dels anys que porta treballant a l’empresa:
+
+Entre 0 i 1 anys -> Auxiliar
+Entre 2 i 10 anys -> Oficial de Segona
+Entre 11 i 20 Anys -> Oficial de Primera
+Més de 20 anys -> Que es jubili!
+
+
+Exercici 7 - Fes una consulta utilitzant la funció anterior perquè mostri mostri de cada
+empleat, el codi d’empleat, el nom, els anys treballats i la categoria professional a la que
+pertany.
+
+*/
+
+
+
